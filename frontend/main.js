a4ca50b1bf9e55af9bc9966beb0dafe0ad0a22ed
mehedi02 <mehedi1670@gmail.com>
2022-02-12 12:18:53 +0600

    updated main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount()
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter'
+const functionApiUrl = 'https://getmyresumecounter.azurewebsites.net/api/GetResumeCounter?code=lgiQjdl4Vb55Yl0i35nhLnmalOy5Iu0nqxv1Gu1Ojp84OMnm7ulZvw=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter'
 
 const getVisitCount = () => {
     let count = 30
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.")
